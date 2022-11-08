76cf6f35dbb2777823f568b208695a979abf0abf
KittenCandie <makayla.s.schatz@gmail.com>
2022-11-08 13:26:52 -0600

    Create functions sumAll() and leapYears().

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,37 @@
-const sumAll = function() {
+const sumAll = function(range1, range2) {
+//Figure out which number is the smallest and which is largest. Put in order (first, last)
+        let first;
+        let last;
+    //take the numbers given, and ensure number and positive
+    if ((typeof range1 === "number" && typeof range2 === "number") && (range1 >= 0 && range2 >= 0)){
+        //assign smallest value "first" and largest "last"
+        if (range1 < range2) {
+        first = range1;
+        last = range2;
+        }
+        else if (range2 < range1) {
+            first = range2;
+            last = range1;
+        }
+    }
+    else {
+        return "ERROR"
+    }
 
-};
+
+//Loop to sumAll, return sum
+    let i = first;
+    let sum = i;
+    do {
+        sum += ++i;
+    }
+      while (i < last) 
+       return sum;
+   };
+
+
+sumAll(123, 1)
+        //console.log()
 
 // Do not edit below this line
 module.exports = sumAll;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // if year can be divided by 4 or 400, leap year (= true)
+    //if year can be divided by 100 but NOT 400, not leap year (= false)
+
+    if ((year % 100 === 0 && year % 400 !== 0) || year % 4 !== 0) {
+        return false;
+    }
+    else {
+        return true;
+    }
 
 };
 
